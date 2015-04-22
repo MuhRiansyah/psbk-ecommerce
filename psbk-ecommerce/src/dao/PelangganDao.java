5d68d0fc94df891e941c6bd5999821f911104815
riansyahPC <muh-riansyah@mail.unpas.ac.id>
2015-04-22 08:29:25 +0700

    Penambahan kelas dao Pelanggan

--- psbk-ecommerce/src/dao/PelangganDao.java
+++ psbk-ecommerce/src/dao/PelangganDao.java
@@ -4,4 +4,10 @@ public class PelangganDao {
 	public static void main(String[] args) {
 		System.out.println("hallo");
 	}
+
+	public void mencetakHello() {
+		System.out.println("hello");
+		PelangganDao dao = new PelangganDao();
+	}
+
 }
